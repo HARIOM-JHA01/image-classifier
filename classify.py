a8fc497c0aa0340cf670ca28373299e2c2bbae45
HARIOM-JHA01 <96457683+HARIOM-JHA01@users.noreply.github.com>
2023-12-15 16:22:46 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -52,4 +52,4 @@ def classify_image():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
