66bca366e7b08bd265f76ba6cb167944f638b777
HARIOM-JHA01 <96457683+HARIOM-JHA01@users.noreply.github.com>
2023-12-19 23:41:19 +0530

    Update trying.py

--- trying.py
+++ trying.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import numpy as np
 import tensorflow as tf
+import tensorflow_hub as hub
 
 app = FastAPI()
 
@@ -16,7 +17,12 @@ app.add_middleware(
 )
 
 # Load pre-trained object detection model
-model = tf.saved_model.load("path/to/efficientdet_model")  # Replace with the actual path
+efficientdet_url = "https://tfhub.dev/tensorflow/efficientdet/d0/1"
+model = hub.load(efficientdet_url)
+
+# Load COCO class names
+coco_class_names_url = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
+coco_class_names = tf.keras.utils.get_file("coco_class_names.json", coco_class_names_url)
 
 # Function to perform image detection
 def detect_objects(image_array):
@@ -30,6 +36,12 @@ def preprocess_image(img_path):
     img_array = np.array(img) / 255.0  # Normalize pixel values to [0, 1]
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
+
+# Function to get COCO category names
+def get_coco_category_name(class_label):
+    with open(coco_class_names, "r") as f:
+        class_names = f.read().splitlines()
+    return class_names[class_label]
 
 @app.post("/detect_objects")
 async def detect_objects_route(file: UploadFile = File(...)):
@@ -45,13 +57,30 @@ async def detect_objects_route(file: UploadFile = File(...)):
         # Perform object detection
         detections = detect_objects(image_array)
 
-        # Process the detection results as needed
-        # For simplicity, returning the number of detected objects
-        num_objects = int(detections["num_detections"].numpy())
-        
+        # Process the detection results
+        detected_objects = []
+        for i in range(int(detections["num_detections"].numpy())):
+            class_label = int(detections["detection_classes"][0, i].numpy())
+            confidence = float(detections["detection_scores"][0, i].numpy())
+            bounding_box = detections["detection_boxes"][0, i].numpy()
+
+            coco_category_name = get_coco_category_name(class_label)
+
+            if confidence > 0.5:
+                print(f"Detected class {coco_category_name} with confidence {confidence}")
+                print("Bounding box coordinates:", bounding_box)
+
+                detected_objects.append({
+                    'class': coco_category_name,
+                    'confidence': confidence,
+                    'bbox': bounding_box.tolist()
+                })
+
+        num_objects = len(detected_objects)
+
         # Remove the temporary image
         tf.io.gfile.remove(img_path)
 
-        return {"result": f"Detected {num_objects} objects"}
+        return {"result": f"Detected {num_objects} objects", "objects": detected_objects}
     except Exception as e:
         return HTTPException(detail=str(e), status_code=500)
